18cb16a5de8e1fd23d71ab955394f388c0386009
aishanirfan <aishanirfan6@gmail.com>
2024-07-23 16:26:42 +0530

    added the diff layouts for the mobile and desktop view for the learn page

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,12 +1,22 @@
+import { MobileHeader } from "@/components/mobile-header";
+import { SideBar } from "@/components/sidebar";
+
 type Props ={
     children : React.ReactNode
 }
 
 const MainLayout = ({ children }: Props) => {
   return ( 
-    <div>
-        {children}
-    </div>
+    <>
+    <MobileHeader />
+    <SideBar className="hidden lg:flex"/>
+        <main className="lg:pl-[256px] h-full pt-[50px] lg:pt-0">
+            <div className="bg-red-500 h-full">
+            {children}
+            </div>
+        </main>
+       
+    </>
 )
 };
 
