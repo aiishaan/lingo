eba7377c7267f25dd01ffd1cbc1903b41a21fdc1
aishanirfan <aishanirfan6@gmail.com>
2024-07-24 16:47:47 +0530

    added user progress server action

--- app/(main)/courses/page.tsx
+++ app/(main)/courses/page.tsx
@@ -3,8 +3,10 @@ import { List } from "./list";
 
 const CoursesPage = async() => {
 
-    const courses = await getCourses();
-    const userProgress = await getUserProgress();
+    const coursesPromise =  getCourses();
+    const userProgressPromise =  getUserProgress();
+
+    const [courses, userProgress] = await Promise.all([coursesPromise, userProgressPromise]);
     return( 
         <div className="h-full max-w-[912px] px-3 mx-auto">
             <h1 className="text-2xl font-bold text-neutral-700">
