36dc081e9e4174176d551e374fd1386c8c5e75f9
aishanirfan <aishanirfan6@gmail.com>
2024-07-24 17:01:05 +0530

    finished updating the selected course progress

--- app/(main)/courses/list.tsx
+++ app/(main)/courses/list.tsx
@@ -5,6 +5,7 @@ import { Card } from "./card"
 import { useRouter } from "next/navigation"
 import { useTransition } from "react"
 import { upsertUserProgress } from "@/actions/user-progress"
+import { toast } from "sonner"
 
 type Props = {
     courses : typeof courses.$inferSelect[],
@@ -23,13 +24,13 @@ export const List = ({courses, activeCourseId}:Props) => {
         }
 
         startTransition(()=>{
-            upsertUserProgress(id);
+            upsertUserProgress(id).catch(()=>toast.error("Failed to update user progress"))
         })
     }
     return (
         <div className="pt-6 grid grid-cols-2 lg:grid-cols-[repeat(auto-fill,minmax(210px,1fr))] gap-4">
             {courses.map((course)=>(
-                <Card key={course.id} id={course.id} title={course.title} imageSrc={course.imageSrc} onClick={onClick} disabled = {false} active={course.id === activeCourseId}/>
+                <Card key={course.id} id={course.id} title={course.title} imageSrc={course.imageSrc} onClick={onClick} disabled = {pending} active={course.id === activeCourseId}/>
             ))}
         </div>
     )
