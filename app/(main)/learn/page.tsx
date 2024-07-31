72aa40bdb515d394dfd2881ee25911e2253d8fe4
aishanirfan <aishanirfan6@gmail.com>
2024-07-31 16:11:43 +0530

    added courseProgress and lessonPercentage to the learn page

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -2,18 +2,24 @@ import { FeedWrapper } from "@/components/feed-wrapper";
 import { StickyWrapper } from "@/components/sticky-wrapper";
 import { Header } from "./header";
 import { UserProgress } from "@/components/user-progress";
-import { getUnits, getUserProgress } from "@/db/queries";
+import { getCourseProgress, getLessonPercentage, getUnits, getUserProgress } from "@/db/queries";
 import { redirect } from "next/navigation";
 import { Unit } from "./unit";
 
 const LearnPage = async () => {
 
   const userProgresPromise = getUserProgress()
+  const courseProgressPromise = getCourseProgress();
+  const lessonPercentagePromise = getLessonPercentage();
   const unitsData = getUnits();
 
-  const [userProgress, units] = await Promise.all([userProgresPromise, unitsData])
+  const [userProgress, units, courseProgress, lessonPercentage] = await Promise.all([userProgresPromise, unitsData, courseProgressPromise, lessonPercentagePromise])
 
   if(!userProgress || !userProgress.activeCourse){
+    redirect("/courses")
+  }
+
+  if(!courseProgress){
     redirect("/courses")
   }
   return (
@@ -25,7 +31,7 @@ const LearnPage = async () => {
         <Header title ={userProgress.activeCourse.title}/>
         {units.map((unit)=>(
           <div key={unit.id} className="mb-10">
-            <Unit id={unit.id} order = {unit.order} description = {unit.description} title={unit.title} lessons = {unit.lessons} activeLesson= {undefined} activeLessonPercentage={0}/>
+            <Unit id={unit.id} order = {unit.order} description = {unit.description} title={unit.title} lessons = {unit.lessons} activeLesson= {courseProgress.activeLesson} activeLessonPercentage={lessonPercentage}/>
           </div>
         ))}
       </FeedWrapper>
