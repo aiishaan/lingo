75b77c5b7a4336b777999497cc7f68f3f6d536f1
aishanirfan <aishanirfan6@gmail.com>
2024-08-06 17:04:01 +0530

    added the continue button

--- app/lesson/quiz.tsx
+++ app/lesson/quiz.tsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 import { Header } from "./header";
 import { QuestionBubble } from "./question-bubble";
 import { Challenge } from "./challenge";
+import { Footer } from "./footer";
 
 type Props = {
     initialPercentage: number;
@@ -29,8 +30,19 @@ export const Quiz = ({
         return uncompletedIndex === -1 ? 0 : uncompletedIndex;
     });
 
+    const [selectedOption, setSelectedOption] = useState<number>();
+    const [status , setStatus] = useState<"correct" | "wrong" | "none">("none");
+
+
     const challenge = challenges[activeIndex];
     const options = challenge?.challengeOptions ?? [];
+
+
+    const onSelect = (id: number)=> {
+        if(status!= "none") return;
+
+        setSelectedOption(id);
+    }
 
     const title = challenge.type === "ASSIST" ? "Select the correct meaning" : challenge.question;
 
@@ -53,9 +65,9 @@ export const Quiz = ({
                             )}
                             <Challenge 
                                 options={options} 
-                                onSelect={()=> {}}
-                                status="none"
-                                selectedOption={undefined}
+                                onSelect={onSelect}
+                                status={status}
+                                selectedOption={selectedOption}
                                 disabled={false}
                                 type={challenge.type}
                             />
@@ -63,6 +75,7 @@ export const Quiz = ({
                     </div>
                 </div>
             </div>
+            <Footer disabled={!selectedOption} status={status} onCheck={()=> {}}/>
         </>
     )
 }
