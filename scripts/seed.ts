70543843e4405cda8759fa2f166fb82d38c94d2d
aishanirfan <aishanirfan6@gmail.com>
2024-08-07 17:36:08 +0530

    added new challenges

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -181,6 +181,31 @@ const main = async () => {
             },
         ]);
 
+        await db.insert(schema.challenges).values([
+            {
+                id: 4,
+                lessonId :2, //Verbs
+                type : "SELECT",
+                order : 1,
+                question : 'Which one of these is "the man"?',
+            },
+            {
+                id: 5,
+                lessonId :2, //Verbs
+                type : "ASSIST",
+                order : 2,
+                question : '"the man"',
+            },
+            {
+                id: 6,
+                lessonId :2, //verbs
+                type : "SELECT",
+                order : 3,
+                question : 'Which one of these is "the robot"?',
+            },
+            
+        ]);
+
         console.log("Seeding database... done");
 
     } catch (err) {
