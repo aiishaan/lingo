f88ff1faec05bb55990600f5f954d2e56a5a629a
aishanirfan <aishanirfan6@gmail.com>
2024-08-09 12:27:49 +0530

    added backend server action for buying hearts in the shop

--- app/(main)/shop/items.tsx
+++ app/(main)/shop/items.tsx
@@ -1,7 +1,12 @@
 "use client";
 
+import { refillHearts } from "@/actions/user-progress";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import { useTransition } from "react";
+import { toast } from "sonner";
+
+const POINTS_TO_REFILL = 10;
 
 type Props = {
     hearts : number;
@@ -11,6 +16,19 @@ type Props = {
 
 
 export const Items = ({hearts, points, hasActiveSubscription}:Props)=> {
+
+    const [pending, startTransition] = useTransition();
+
+    const onRefillHearts = () => {
+        if(pending || hearts === 5 || points < POINTS_TO_REFILL){
+            return;
+        }
+
+        startTransition(()=> {
+            refillHearts().catch(()=> toast.error("Something went wrong"));
+        })
+    }
+
     return(
         <ul className="w-full">
             <div className="flex items-center w-full p-4 gap-x-4 border-t-2">
@@ -20,12 +38,12 @@ export const Items = ({hearts, points, hasActiveSubscription}:Props)=> {
                         Refill hearts
                     </p>
                 </div>
-                <Button disabled={hearts === 5}>
+                <Button onClick={onRefillHearts} disabled={hearts === 5 || points < POINTS_TO_REFILL || pending}>
                     {hearts === 5 ? "full" : (
                         <div className="flex items-center">
                             <Image src="/points.svg" alt="Points" height={20} width={20}/>
                             <p>
-                                50
+                                {POINTS_TO_REFILL}
                             </p>
                         </div>
                     )}
