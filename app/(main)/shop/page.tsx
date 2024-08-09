f6dcec3b4640a708255cc1cbb04b2fc75d609ec7
aishanirfan <aishanirfan6@gmail.com>
2024-08-09 12:08:01 +0530

    added hearts to the shop

--- app/(main)/shop/page.tsx
+++ app/(main)/shop/page.tsx
@@ -1,7 +1,10 @@
+import { FeedWrapper } from "@/components/feed-wrapper";
 import { StickyWrapper } from "@/components/sticky-wrapper"
 import { UserProgress } from "@/components/user-progress"
 import { getUserProgress } from "@/db/queries"
+import Image from "next/image";
 import { redirect } from "next/navigation";
+import { Items } from "./items";
 
 const ShopPage = async() => {
 
@@ -21,6 +24,17 @@ const ShopPage = async() => {
             <StickyWrapper>
                 <UserProgress activeCourse={userProgress.activeCourse} hearts={userProgress.hearts} points={userProgress.points} hasActiveSubscription={false}/>
             </StickyWrapper>
+
+            <FeedWrapper>
+                <div className="w-full flex flex-col items-center">
+                    <Image src="/shop.svg" alt="Shop" height={90} width={90}/>
+                    <h1 className="text-center font-bold text-neutral-800 text-2xl my-6">Shop</h1>
+                    <p className="text-muted-foreground text-center text-lg mb-6">
+                        Spend your points on cool stuff.
+                    </p>
+                    <Items hearts={userProgress.hearts} points={userProgress.points} hasActiveSubscription={false} /> {/*todo add subs*/}
+                </div>
+            </FeedWrapper>
         </div>
     )
 }
